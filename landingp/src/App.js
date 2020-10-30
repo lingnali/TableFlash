5b4c129083c27fd3f1adccec734c66a6cfa64b14
Lingna Li <lingnali@Lingnas-MacBook-Pro-2020.local>
2020-10-30 14:55:38 -0700

    Rename folder

---
+++ landingp/src/App.js
@@ -0,0 +1,205 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useHistory,
+  useLocation,
+  useParams
+} from "react-router-dom";
+
+import styled from 'styled-components';
+
+
+// This example shows how to render two different screens
+// (or the same screen in a different context) at the same URL,
+// depending on how you got there.
+//
+// Click the "featured images" and see them full screen. Then
+// "visit the gallery" and click on the colors. Note the URL and
+// the component are the same as before but now we see them
+// inside a modal on top of the gallery screen.
+
+export default function ModalGalleryExample() {
+  return (
+    <Router>
+      <ModalSwitch />
+    </Router>
+  );
+}
+
+function ModalSwitch() {
+  let location = useLocation();
+
+  // This piece of state is set when one of the
+  // gallery links is clicked. The `background` state
+  // is the location that we were at when one of
+  // the gallery links was clicked. If it's there,
+  // use it as the location for the <Switch> so
+  // we show the gallery in the background, behind
+  // the modal.
+  let background = location.state && location.state.background;
+
+  return (
+    <div>
+      <Switch location={background || location}>
+        <Route exact path="/" children={<Home />} />
+        <Route path="/gallery" children={<Gallery />} />
+        <Route path="/img/:id" children={<ImageView />} />
+      </Switch>
+
+      {/* Show the modal when a background page is set */}
+      {background && <Route path="/img/:id" children={<Modal />} />}
+    </div>
+  );
+}
+
+// created style component
+const Image = styled.div`
+  width: 400px;
+  height: 400px;
+  background: no-repeat center/150% url(/img/${({index}) => index}.png);
+`
+
+const IMAGES = [
+  { id: 1, title: "Blueberry" },
+  { id: 2, title: "House" },
+  { id: 3, title: "Girl"},
+  { id: 4, title: "Bull" },
+  { id: 5, title: "Palm"},
+  { id: 6, title: "Cat"},
+  { id: 7, title: "Camera" },
+  { id: 8, title: "Compass" },
+  { id: 9, title: "Fire" },
+  { id: 10, title: "Wave" }  ,
+  { id: 11, title: "Coffee" },
+  { id: 12, title: "Stick Man" },
+  { id: 13, title: "Mountain" },
+  { id: 14, title: "Succulent" },
+  { id: 15, title: "Barn" }
+];
+
+// function Thumbnail({ color }) {
+//   return (
+//     <div
+//       style={{
+//         width: 50,
+//         height: 50,
+//         background: color
+//       }}
+//     />
+//   );
+// }
+
+// function Image({ color }) {
+//   return (
+//     <div
+//       style={{
+//         width: "100%",
+//         height: 400,
+//         background: color
+//       }}
+//     />
+//   );
+// }
+
+function Home() {
+  return (
+    <div>
+      <Link to="/gallery">Visit the Gallery</Link>
+      <h2>Featured Images</h2>
+      <ul>
+        <li>
+          <Link to="/img/2">Tomato</Link>
+        </li>
+        <li>
+          <Link to="/img/4">Crimson</Link>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+function Gallery() {
+  let location = useLocation();
+
+  return (
+    <div>
+      {IMAGES.map(i => (
+        <Link
+          key={i.id}
+          to={{
+            pathname: `/img/${i.id}`,
+            // This is the trick! This link sets
+            // the `background` in location state.
+            state: { background: location }
+          }}
+        >
+          <Image index={i.id} />
+          <p>{i.title}</p>
+        </Link>
+      ))}
+    </div>
+  );
+}
+
+function ImageView() {
+  let { id } = useParams();
+  let image = IMAGES[parseInt(id, 10) - 1];
+
+  if (!image) return <div>Image not found</div>;
+
+  return (
+    <div>
+      <h1>{image.title}</h1>
+      <Image index={image.id} />
+    </div>
+  );
+}
+
+function Modal() {
+  let history = useHistory();
+  let { id } = useParams();
+  let image = IMAGES[parseInt(id, 10) - 1];
+
+  if (!image) return null;
+
+  let back = e => {
+    e.stopPropagation();
+    history.goBack();
+  };
+
+  return (
+    <div
+      onClick={back}
+      style={{
+        position: "absolute",
+        top: 0,
+        left: 0,
+        bottom: 0,
+        right: 0,
+        background: "rgba(0, 0, 0, 0.15)"
+      }}
+    >
+      <div
+        className="modal"
+        style={{
+          position: "absolute",
+          background: "#fff",
+          top: 25,
+          left: "10%",
+          right: "10%",
+          padding: 15,
+          border: "2px solid #444"
+        }}
+      >
+        <h1>{image.title}</h1>
+        <Image index={image.id} />
+        <button type="button" onClick={back}>
+          Close
+        </button>
+      </div>
+    </div>
+  );
+}
